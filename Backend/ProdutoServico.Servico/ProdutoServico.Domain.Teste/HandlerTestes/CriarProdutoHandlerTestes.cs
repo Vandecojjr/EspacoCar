1dff28638124993c1c0807bf3275b0652a5bc4fc
vanderlei <junior_12morais@hotmail.com>
2024-07-29 09:29:40 -0400

    modified:   Backend/ProdutoServico.Servico/ProdutoServico.Domain.Api/obj/Debug/net8.0/ProdutoServico.Domain.Api.AssemblyInfo.cs 	modified:   Backend/ProdutoServico.Servico/ProdutoServico.Domain.Api/obj/Debug/net8.0/ProdutoServico.Domain.Api.AssemblyInfoInputs.cache 	modified:   Backend/ProdutoServico.Servico/ProdutoServico.Domain.Api/obj/Debug/net8.0/ProdutoServico.Domain.Api.csproj.AssemblyReference.cache 	modified:   Backend/ProdutoServico.Servico/ProdutoServico.Domain.Infra/obj/Debug/net8.0/ProdutoServico.Domain.Infra.AssemblyInfo.cs 	modified:   Backend/ProdutoServico.Servico/ProdutoServico.Domain.Infra/obj/Debug/net8.0/ProdutoServico.Domain.Infra.AssemblyInfoInputs.cache 	modified:   Backend/ProdutoServico.Servico/ProdutoServico.Domain.Infra/obj/Debug/net8.0/ProdutoServico.Domain.Infra.csproj.AssemblyReference.cache 	modified:   Backend/ProdutoServico.Servico/ProdutoServico.Domain.Teste/HandlerTestes/CriarProdutoHandlerTestes.cs 	new file:   Backend/ProdutoServico.Servico/ProdutoServico.Domain.Teste/Repositories/FalsoProdutoRepository.cs

--- Backend/ProdutoServico.Servico/ProdutoServico.Domain.Teste/HandlerTestes/CriarProdutoHandlerTestes.cs
+++ Backend/ProdutoServico.Servico/ProdutoServico.Domain.Teste/HandlerTestes/CriarProdutoHandlerTestes.cs
@@ -1,23 +1,28 @@
 using ProdutoServico.Domain.Commands;
 using ProdutoServico.Domain.Handlers;
+using ProdutoServico.Domain.Teste.Repositories;
 
 namespace ProdutoServico.Domain.Teste.HandlerTestes
 {
     [TestClass]
     public class CriarProdutoHandlerTestes
     {
+        private readonly CriarProdutoCommand _comandoInvalido = new(" ", 0, 0, 0);
+        private readonly CriarProdutoCommand _comandoValido = new("Vela", 15, 10.00m, 50.00m);
+        private readonly ProdutoHandler _manipulador = new ProdutoHandler(new FalsoProdutoRepository());
+        private GenericoCommandResult _resultado = new GenericoCommandResult();
 
         [TestMethod]
         public void Dado_um_comando_invalido_deve_interromper_a_execucao()
         {
-            var comando = new CriarProdutoCommand("", 0, 0, 0);
-            var manipulador = new ProdutoHandler(null);
-            Assert.Fail();
+            _resultado = (GenericoCommandResult)_manipulador.Handle(_comandoInvalido);
+            Assert.AreEqual(_resultado.Sucesso, false);
         }
         [TestMethod]
         public void Dado_um_comando_valido_deve_criar_o_produto()
         {
-            Assert.Fail();
+            _resultado = (GenericoCommandResult)_manipulador.Handle(_comandoValido);
+            Assert.AreEqual(_resultado.Sucesso, true);
         }
     }
 }
---
+++ Backend/ProdutoServico.Servico/ProdutoServico.Domain.Teste/Repositories/FalsoProdutoRepository.cs
@@ -0,0 +1,19 @@
+using ProdutoServico.Domain.Entities;
+using ProdutoServico.Domain.Repositories;
+
+namespace ProdutoServico.Domain.Teste.Repositories
+{
+    public class FalsoProdutoRepository : IProdutoRepository
+    {
+        public void Atualizar(Produto produto)
+        {
+
+        }
+
+        public void Criar(Produto produto)
+        {
+
+        }
+
+    }
+}
